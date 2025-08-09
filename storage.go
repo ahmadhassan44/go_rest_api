c71bb10824f89f76d95adec05c381151ea2d389c
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2025-08-09 22:10:41 +0500

    feat/add table creation query

--- storage.go
+++ storage.go
@@ -22,6 +22,41 @@ type PostGresStore struct {
 	db *sql.DB
 }
 
+func NewPostgresStore() (*PostGresStore, error) {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbPort := os.Getenv("DB_PORT")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s dbname=gobank password=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return &PostGresStore{db: db}, nil
+}
+
+func (pgStore *PostGresStore) Init() error {
+	return pgStore.createAccountTable()
+}
+
+func (pgStore *PostGresStore) createAccountTable() error {
+	query := `CREATE TABLE IF  NOT EXISTS account(
+		id varchar(36) primary key,
+		first_name varchar(100),
+		last_name varchar(100),
+		number int,
+		balance int,
+		created_at timestamp,
+		updated_at timestamp
+	)`
+	_, err := pgStore.db.Exec(query)
+	return err
+}
+
 func (pgStore *PostGresStore) CreateAccount(*Account) error {
 	return nil
 
@@ -36,22 +71,5 @@ func (pgStore *PostGresStore) UpdateAccount(*Account) error {
 }
 func (pgStore *PostGresStore) GetAccountById(id string) (*Account, error) {
 	return nil, nil
-
-}
-
-func NewPostgresStore() (*PostGresStore, error) {
-	dbHost := os.Getenv("DB_HOST")
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
-
-	connStr := fmt.Sprintf("host=%s user=%s dbname=gobank password=%s sslmode=disable", dbHost, dbUser, dbPass)
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		return nil, err
-	}
-	if err := db.Ping(); err != nil {
-		return nil, err
-	}
-	return &PostGresStore{db: db}, nil
 
 }
